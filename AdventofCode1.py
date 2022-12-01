e9928b2519a09fa936064edf175a770cb4f619e3
Joe Nicholson <73400689+Joe-Nicholson21@users.noreply.github.com>
2022-12-01 16:49:49 +0000

    Add files via upload

---
+++ AdventofCode1.py
@@ -0,0 +1,13 @@
+def Day_One(data):
+    elves = [i for i in data.strip().split("\n\n")]
+    elf_calories = [sum([int(cals)for cals in elf.split()]) for elf in elves]
+    sorted_elves = sorted(elf_calories)
+    return max(elf_calories),sum(sorted_elves[-3:])
+    
+    
+
+
+if __name__ == '__main__':
+    data = open("day1.txt").read()
+    part1,part2=Day_One(data)
+    print("Part1: ", part1, "\nPart2:", part2)dv
